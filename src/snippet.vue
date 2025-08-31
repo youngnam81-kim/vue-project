b770a1eef1ee4710958cab009b3eb34909c57d02
youngnam81-kim <kimyn81@naver.com>
2025-08-31 23:32:39 +0900

    .

---
+++ src/snippet.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: '${TM_FILENAME_BASE}',
+    components: { //외부 컴포넌트는 import 한후 여기 등록해서 사용.
+
+    },
+    data() { //html과 자바스크립트에서 사용할 데이터 전역 변수 this.sampleData 으로 접근
+        return {
+            sampleData: '',
+        };
+    },
+    setup() { //컴포지션 API
+
+    },
+    beforeRouteEnter(){
+
+    },
+    beforeCreate(){
+
+    },
+    created() { //컴포넌트가 생성 되면 실행
+
+    },
+    beforeMount(){
+
+    },
+    mounted() { //template에 정의된 html 코드가 랜더링된 후 실행
+
+    },
+    beforeUpdate(){
+
+    },
+    updated(){
+
+    },
+    unmounted() { //컴포넌트가 unmount가 완료된 후 실행
+
+    },
+    beforeRouteLeave(){
+
+    },
+    methods: { //컴포넌
+    // 트 내에서 사용할 메소드 정의 this. 으로 접근
+        sample() {
+
+        },
+        sample2() {
+
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
